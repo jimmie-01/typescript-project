4998f0aff3895bd7f6389d1a333dcd3a21d684ce
Fakile Razaq <razaq_jimmie@yahoo.com>
2025-11-25 21:38:33 +0100

    TypeScript TODO List Project

---
+++ src/model/FullList.ts
@@ -0,0 +1,27 @@
+import ListItem from "./ListItem";
+
+interface List {
+	list: ListItem[],
+	load(): void,
+	save(): void,
+	clearList(): void,
+	addItem(itemObj: ListItem): void,
+	removeItem(id: string): void
+}
+
+export default class FullList implements List {
+
+	static instance: FullList = new FullList()
+
+	private constructor (private _list: ListItem[] = []){}
+
+	get list(): ListItem[] {
+		return this._list;
+	}
+
+	set list(list: ListItem[]) {
+		this._list = list;
+	}
+
+	
+} 
