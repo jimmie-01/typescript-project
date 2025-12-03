509abb564ab213911aeb7e82f7034c0fe9c390d9
Fakile Razaq <razaq_jimmie@yahoo.com>
2025-12-03 19:38:19 +0100

    Basic crud feature implemented

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,26 @@ const initApp = (): void => {
 
 	itemEntryForm.addEventListener("submit", (event: SubmitEvent): void => {
 		event.preventDefault();
+
+		const input = document.getElementById("newItem") as HTMLInputElement;
+
+		const newEntryText: string = input.value.trim();
+
+		if(!newEntryText) return;
+
+		// Calculate ItemId
+		const itemId: number = fullList.list.length
+		? parseInt((fullList.list[fullList.list.length - 1]).id) + 1 : 1;
+
+		// Create new list entry with newEntryText and itemId
+		const newItem = new ListItem(itemId.toString(), newEntryText);
+
+		// Add new item to the fullList
+		fullList.addItem(newItem);
+
+		// Render fullList with new item
+		template.render(fullList);
+
 	});
 
 	const clearItems = document.getElementById("clearItemsButton") as HTMLButtonElement;
